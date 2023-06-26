dc3ca9f9cea559d32fd9a59b49a7f53d6d654947
Mohit Kumar <mohitbro96@gmail.com>
2023-06-26 21:56:59 +0530

    Add Hero Section

---
+++ src/components/GetStartedButton.jsx
@@ -0,0 +1,30 @@
+import { arrowUp } from "../assets";
+import styles from "../style";
+
+const GetStartedButton = () => {
+  return (
+    <div
+      className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}
+    >
+      <div
+        className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}
+      >
+        <div className={`${styles.flexStart} flex-row`}>
+          <p className="font-poppins font-medium text-[18px] leading-[23px] flex flex-row ">
+            <span className="text-gradient mr-1">Get</span>
+            <img
+              src={arrowUp}
+              alt="arrow_up"
+              className="w-[23px] h-[23px] object-contain"
+            />
+          </p>
+        </div>
+        <p className="font-poppins font-medium text-[18px] leading-[23px]">
+          <span className="text-gradient">Started</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default GetStartedButton;
