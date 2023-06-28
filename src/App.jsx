4681a93365b32a056a1359913baff89f86f47f2d
Mohit Kumar <mohitbro96@gmail.com>
2023-06-28 22:59:38 +0530

    Add all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,41 @@
+import Billing from "./components/Billing";
+import Business from "./components/Business";
+import CTA from "./components/CTA";
+import CardDeal from "./components/CardDeal";
+import Clients from "./components/Clients";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import { Stats } from "./components/Stats";
+import Testimonials from "./components/Testimonials";
+import styles from "./style";
 const App = () => {
-  return <div className="bg-black">App</div>;
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+      <div className={`${styles.flexStart} bg-primary`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+      <div className={`${styles.flexStart} ${styles.paddingX} bg-primary`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
